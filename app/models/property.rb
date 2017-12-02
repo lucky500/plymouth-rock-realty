b9b64988d93f98f1271042debc651c370a915d61
Luciano Oliveira <luciano101@msn.com>
2017-12-02 15:07:21 -0800

    property rspec testing all passing

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,7 @@
 class Property < ApplicationRecord
   validates :address, presence: true, length: {minimum: 10}
   validates :price, presence: true
+  validates :description, presence: true
   validates :bedrooms, presence: true
   validates :bathrooms, presence: true
   validates :type, presence: true
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -4,8 +4,43 @@ RSpec.describe Property, type: :model do
   context 'validation tests' do
     let(:property) { build(:property) }
     it 'ensures presence of address' do
-      property.address = nil
+      expect(property.address).to(eq('295 O Littleton County Rd, Harvard, MA 01451'))
+    end
+
+    it 'ensures presence of price' do
+      expect(property.price).to(eq('575,000'))
+    end
+
+    it 'ensures presence of description' do
+      property.description = nil
       expect(property.save).to(eq(false))
     end
+
+    it 'ensures presence of bedrooms' do
+      expect(property.bedrooms).to(eq('4'))
+    end
+
+    it 'ensures presence of bathrooms' do
+      property.bathrooms = '3'
+      expect(property.bathrooms).to(eq('3'))
+    end
+
+    it 'ensures presence of property type' do
+      expect(property.type).to(eq('Single Family'))
+    end
+
+    it 'ensures presence of property sqft' do
+      property.sqft = nil
+      expect(property.save).to(eq(false))
+    end
+
+    it 'ensures presence of lot size' do
+      expect(property.lot).to(eq('6.92'))
+    end
+
+    it 'ensures presence of property age' do
+      expect(property.year_built).to(eq('1974'))
+    end
+
   end
 end
