ac5e344c7d5dfad0ccad14cd74b37130348656f5
Luciano Oliveira <luciano101@msn.com>
2017-12-04 10:43:21 -0800

    set up comment views, allow user to add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,39 +1,27 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :find_property
 
-  # GET /comments
-  # GET /comments.json
   def index
     @comments = Comment.all
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
   def new
     @comment = Comment.new
   end
 
-  # GET /comments/1/edit
+
   def edit
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    @comment.property_id = @property.id
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to property_path(@property)    
+    else
+      render :new
     end
   end
 
@@ -62,13 +50,15 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
+  def find_property
+    @property = Property.find(params[:property_id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:description, :user_id, :property_id)
-    end
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:description, :user_id, :property_id)
+  end
 end
--- app/views/comments/_comment.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! comment, :id, :description, :user_id, :property_id, :created_at, :updated_at
-json.url comment_url(comment, format: :json)
