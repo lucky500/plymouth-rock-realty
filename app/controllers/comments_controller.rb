3863ed1ca507dffab11a5f4d3c2f765c132415e5
Luciano Oliveira <luciano101@msn.com>
2017-12-04 11:50:53 -0800

    add crud functionality to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,10 +10,6 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
-
-  def edit
-  end
-
   def create
     @comment = Comment.new(comment_params)
     @comment.property_id = @property.id
@@ -25,28 +21,20 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
+  def edit
+  end
+
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.update(comment_params)
+      redirect_to property_path(@property)    
+    else
+      render :edit
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to property_path(@property)
   end
 
   private
